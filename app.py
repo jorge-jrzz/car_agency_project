8b8ce8c9fe3a1bc528332b2258b2a79cd9b6feac
jorge Juarez <jorgeang33@gmail.com>
2024-02-07 01:43:05 -0600

    Add Logout button and redirect root to login

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def root():
+    return redirect(url_for('login'))
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/administrador')
+def admin():
+    return render_template('home_admin.html')
 
 
 if __name__ == '__main__':
