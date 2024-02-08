3f020734cc2e0231bd306251d9305f4cc3dca139
jorge Juarez <jorgeang33@gmail.com>
2024-02-07 19:59:45 -0600

    rename home page

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/administrador')
-def admin():
-    return render_template('home_admin.html')
+@app.route('/home')
+def home():
+    return render_template('home_page.html')
 
 
 if __name__ == '__main__':
